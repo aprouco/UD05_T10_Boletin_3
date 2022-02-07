0e8977f20e09dbece25f79e6578ce98f9183fd20
aprouco <adripenasrouco@gmail.com>
2022-02-07 10:10:54 +0100

    Ejercicio1

--- ejercicio1.html.php
+++ ejercicio1.html.php
@@ -7,36 +7,134 @@
     <title>Boletín03_T01</title>
 </head>
 <body>
-    <form action="ejercicio1.php" method="post">
+    <form action="ejercicio1.html.php" method="post">
         <label for="dia">Escolle o día da semana</label>
         <select name="dia">
-            <option value="0">Luns</option>
-            <option value="1">Martes</option>
-            <option value="2">Mércores</option>
-            <option value="3">Xoves</option>
-            <option value="4">Venres</option>
+            <option value="">Escolle unha opción</option>
+           
+            <?php 
+            if (isset($_REQUEST["dia"]) && ($_REQUEST["dia"]==0)){
+                echo '<option value="0" selected>Luns</option>' ;
+            }else{
+                echo '<option value=0>Luns</option>';
+            }
+            ?> 
+            
+            <?php 
+            if (isset($_REQUEST["dia"]) && ($_REQUEST["dia"]==1)){
+                echo '<option value="1" selected>Martes</option>' ;
+            }else{
+                echo '<option value=1>Martes</option>';
+            }
+            ?> 
+            
+            <?php 
+            if (isset($_REQUEST["dia"]) && ($_REQUEST["dia"]==2)){
+                echo '<option value="2" selected>Mércores</option>' ;
+            }else{
+                echo '<option value=2>Mércores</option>';
+            }
+            ?> 
+            
+            <?php 
+            if (isset($_REQUEST["dia"]) && ($_REQUEST["dia"]==3)){
+                echo '<option value="3" selected>Xoves</option>' ;
+            }else{
+                echo '<option value=3>Xoves</option>';
+            }
+            ?> 
+
+            <?php 
+            
+            if (isset($_REQUEST["dia"]) && ($_REQUEST["dia"]==4)){
+                echo '<option value="4" selected>Venres</option>' ;
+            }else{
+                echo '<option value=4>Venres</option>';
+            }
+            ?>
         </select>
         <br>
         <label for="hora">Escolle a hora de clase: </label>
         <select name="hora">
             <option value="">Escolle unha opción</option>
-            <option>8:50 - 9:40</option>
-            <option>9:40 - 10:30</option>
-            <option>10:30 - 11:20</option>
-            <option>12:00 - 12:50</option>
-            <option>12:50 - 13:40</option>
-            <option>13:40 - 14:30</option>
-            <option>16:00 - 16:50</option>
+            <?php 
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="8:50 - 9:40")){
+                echo '<option selected>8:50 - 9:40</option>' ;
+            }else{
+                echo '<option>8:50 - 9:40</option>';
+            }
+            ?>
+
+            <?php 
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="9:40 - 10:30")){
+                echo '<option selected>9:40 - 10:30</option>' ;
+            }else{
+                echo '<option>9:40 - 10:30</option>';
+            }
+            ?>
+            <?php 
+            
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="10:30 - 11:20")){
+                echo '<option selected>10:30 - 11:20</option>' ;
+            }else{
+                echo '<option>10:30 - 11:20</option>';
+            }
+            
+            ?>
+
+            <?php 
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="12:00 - 12:50")){
+                echo '<option selected>12:00 - 12:50</option>' ;
+            }else{
+                echo '<option>12:00 - 12:50</option>';
+            }
+            ?>
+
+            <?php 
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="12:50 - 13:40")){
+                echo '<option selected>12:50 - 13:40</option>' ;
+            }else{
+                echo '<option>12:50 - 13:40</option>';
+            }
+            ?>
+
+            <?php 
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="13:40 - 14:30")){
+                echo '<option selected>13:40 - 14:30</option>' ;
+            }else{
+                echo '<option>13:40 - 14:30</option>';
+            }
+            ?>
+
+            <?php 
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="16:00 - 16:50")){
+                echo '<option selected>16:00 - 16:50</option>' ;
+            }else{
+                echo '<option>16:00 - 16:50</option>';
+            }
+            ?>
             <option>16:50 - 17:40</option>
-            <option>17:40 - 18:30</option>
+            <?php 
+            
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="16:50 - 17:40")){
+                echo '<option selected>16:50 - 17:40</option>' ;
+            }else{
+                echo '<option>16:50 - 17:40</option>';
+            }
+            
+            ?>
+
+            <?php 
+            if (isset($_REQUEST["hora"]) && ($_REQUEST["hora"]=="16:50 - 17:40")){
+                echo '<option selected>17:40 - 18:30</option>' ;
+            }else{
+                echo '<option>17:40 - 18:30</option>';
+            }
+            ?>
         </select>
         <br>
         <input type="submit" value="Enviar"/>
     </form>
-</body>
-</html>
-
-
 
 <?php
 
@@ -54,18 +152,15 @@ $horario = array (
     '17:40 - 18:30' => array('','SRI','','ASO','')
 );
 
-$dia=isset($_REQUEST["dia"])?$_REQUEST ["dia"]:"";
-$hora=isset($_REQUEST["hora"])?$_REQUEST ["hora"]:"";
 
-if ($dia=="" || $hora=="") {
-    echo "<p>Tes que escoller un día e a hora.</p>";
-}else if ($dia<5 && $dia>=0 && $hora!="") {
-    echo "<p>A hora escollida é $hora.</p>";
-    echo "<p>O día escollido é $dia.</p>";
+if (!isset($_REQUEST["dia"]) || !isset($_REQUEST["dia"]) ) {
+    echo "<p>Tes que escoller o día e a hora.</p>";
+}else{
+    $dia = $_REQUEST["dia"];
+    $hora = $_REQUEST["hora"];
     $horarioDaHoraEscollida = $horario[$hora];
     echo "<p>Nese horario a clase é: ".$horarioDaHoraEscollida[$dia]."</p>";
-}else {
-    echo "<p>O formato da hora ou do día non é correcto.</p>";
 }
-
 ?>
+</body>
+</html>
