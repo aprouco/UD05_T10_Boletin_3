1a31715b459db6b71d4466433a645854276e8b1e
aprouco <adripenasrouco@gmail.com>
2022-02-16 17:04:13 +0100

    ejercicio17

---
+++ ejercicio17.html.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio17</title>
+</head>
+<body>
+    <form action="ejercicio17.html.php">
+        <p>Introduce un número: <input type="text" name="num"></p>
+        <input type="submit" value="Enviar">
+        <br>
+        <p>
+            <?php 
+                $num=$_REQUEST["num"];
+                $dig=str_split($num);
+                if ($dig <=5) {
+                    echo $dig[0];
+                }else {
+                    echo "El número de cifras debe ser menor o igual a 5";
+                }
+            ?>
+        </p>
+    </form>
+</body>
+</html>
