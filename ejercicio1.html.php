8f8c6992b2d4af0e32c1123adeed3d38ab67d529
aprouco <adripenasrouco@gmail.com>
2022-03-16 17:21:11 +0100

    ejercicio1

--- ejercicio1.html.php
+++ ejercicio1.html.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+
+<!--Escribe un programa que pida por teclado un día de la semana y que diga qué asignatura toca a
+primera hora ese día.-->
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
